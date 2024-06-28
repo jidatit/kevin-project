6c1c1386fcc1a1f6b3803931ecab92790249eb44
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-28 11:04:57 +0500

    Changed the folder structure as client and server, as the node.js and express.js backend is also implemented to fetch the data from Zoho CRM. Implemented the routes at backend to generate grant token, access token and refresh token. After all this, a module route is also created to fetch the data of modules from Zoho CRM.

---
+++ kevin-project/client/src/App.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
+import { useAuth } from '../AuthContext'
+
+import AuthLayout from './auth/Layout'
+import UserSignup from './auth/UserSignup'
+import UserLogin from './auth/UserLogin'
+import AdminLogin from './auth/AdminLogin'
+
+import UserLayout from './user_portal/Layout'
+import UserDashboardHome from './user_portal/pages/Dashboard'
+import UserDashboardProfile from './user_portal/pages/Profile'
+import UserDashboardClients from './user_portal/pages/Clients'
+import UserPasswordChange from './user_portal/components/UserPasswordChange'
+import Test from './user_portal/components/Test';
+
+import AdminLayout from './admin_portal/Layout'
+import AdminDashbaordUsers from './admin_portal/pages/Users'
+import AdminPasswordChange from './admin_portal/components/AdminPasswordChange'
+import AddReferral from './admin_portal/components/AddReferral';
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+
+          <Route path='/' element={<AuthLayout />}>
+            <Route index element={<UserLogin />} />
+            <Route path='signup' element={<UserSignup />} />
+            <Route path='admin' element={<AdminLogin />} />
+          </Route>
+
+          <Route path='/user_portal' element={<UserLayout />}>
+            <Route index element={<UserDashboardHome />} />
+            <Route path='profile' element={<UserDashboardProfile />} />
+            <Route path='clients' element={<UserDashboardClients />} />
+            <Route path='test' element={<Test />} />
+            <Route path='changePassword' element={<UserPasswordChange/>} />
+            <Route path='logout' element={<Logout />} />
+          </Route>
+
+          <Route path='/admin_portal' element={<AdminLayout />}>
+            <Route index element={<AdminDashbaordUsers />} />
+            <Route path='changePassword' element={<AdminPasswordChange/>} />
+            <Route path='addreferral' element={<AddReferral/>} />
+            <Route path='logout' element={<Logout />} />
+          </Route>
+
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App;
+
+function Logout() {
+  const { logout, userType } = useAuth()
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-[#1F2634] bg-opacity-75">
+        <div className='w-[654px] h-[310px] rounded-lg mt-[40px] flex flex-col gap-[23px] justify-center items-center bg-white'>
+          <p className='font-bold text-black text-3xl text-center'>Are you sure you want to logout?</p>
+          <div className='w-[540px] h-[70px] flex flex-row gap-6 justify-center'>
+            {userType === "admin" ? (<button onClick={() => { navigate('/admin_portal') }} className='bg-[#BB000E] rounded-md w-[229px] h-[56px] font-bold text-white'>Cancel</button>)
+              : (<button onClick={() => { navigate('/user_portal') }} className='bg-[#BB000E] rounded-md w-[229px] h-[56px] font-bold text-white'>Cancel</button>)}
+            <button onClick={logout} className='bg-[#059C4B] rounded-md w-[229px] h-[56px] font-bold text-white'>Confirm</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ kevin-project/server/server.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+import tokensRouter from './routes/tokens.mjs';
+
+config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/tokens', tokensRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
