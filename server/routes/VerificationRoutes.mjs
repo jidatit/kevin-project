5fe1eea4bf7b8168e5610783b46ea2f7304dec3e
Zohaib Haider <zebihaider123@gmail.com>
2024-12-02 19:17:32 +0500

    Revert "done custom email verification"
    This reverts commit dcff7e26ec3245285657580cd59eb7fc177f9fce.

--- server/routes/VerificationRoutes.mjs
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { sendVerificationEmail } from "../controllers/VerificationControllers.mjs";
-
-const VerificationRoutes = express.Router();
-
-VerificationRoutes.post("/sendVerifyEmail", sendVerificationEmail);
-export default VerificationRoutes;
--- server/server.mjs
+++ server/server.mjs
@@ -1,25 +1,17 @@
-import express from "express";
-import cors from "cors";
-import { config } from "dotenv";
-import tokensRouter from "./routes/tokens.mjs";
-import VerificationRoutes from "./routes/VerificationRoutes.mjs";
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+import tokensRouter from './routes/tokens.mjs';
 
 config();
 
 const app = express();
 const port = process.env.PORT || 10000;
 
-app.use(
-  cors({
-    origin: [
-      `${process.env.FRONTEND_URL}`, // Add your new frontend URL here
-    ],
-    // credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
-app.use("/api", tokensRouter);
-app.use("/auth", VerificationRoutes);
+app.use('/api', tokensRouter);
+
 app.listen(port, () => {
-  console.log(`Server is running on port : ${port}`);
+    console.log(`Server is running on port : ${port}`);
 });
