2e0dafd246c5356166c6b0b7eceee020cca3fc46
Zubair zahid <zubairzahid228@gmail.com>
2025-04-10 10:48:23 +0500

    update : temp-email-verfied

--- client/AuthContext.jsx
+++ client/AuthContext.jsx
@@ -7,113 +7,114 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-	const [currentUser, setCurrentUser] = useState(null);
-	const [loading, setLoading] = useState(true);
-	const [userType, setUserType] = useState("");
-	const [isEmailVerified, setIsEmailVerified] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userType, setUserType] = useState("");
+  const [isEmailVerified, setIsEmailVerified] = useState(false);
 
-	const getUserDetails = async (userId) => {
-		try {
-			// Check if the user exists in the 'users' collection
-			const userDocRef = doc(db, "users", userId);
-			const userDocSnapshot = await getDoc(userDocRef);
+  const getUserDetails = async (userId) => {
+    try {
+      // Check if the user exists in the 'users' collection
+      const userDocRef = doc(db, "users", userId);
+      const userDocSnapshot = await getDoc(userDocRef);
 
-			if (userDocSnapshot.exists()) {
-				const userData = userDocSnapshot.data();
+      if (userDocSnapshot.exists()) {
+        const userData = userDocSnapshot.data();
 
-				return userData;
-			} else {
-				// Check if the user exists in the 'admins' collection
-				const adminDocRef = doc(db, "admins", userId);
-				const adminDocSnapshot = await getDoc(adminDocRef);
+        return userData;
+      } else {
+        // Check if the user exists in the 'admins' collection
+        const adminDocRef = doc(db, "admins", userId);
+        const adminDocSnapshot = await getDoc(adminDocRef);
 
-				if (adminDocSnapshot.exists()) {
-					const adminData = adminDocSnapshot.data();
+        if (adminDocSnapshot.exists()) {
+          const adminData = adminDocSnapshot.data();
 
-					return adminData;
-				} else {
-					console.log("No user or admin found with the given userId: ", userId);
-					return null;
-				}
-			}
-		} catch (error) {
-			console.error("Error fetching user or admin details: ", error);
-			throw error;
-		}
-	};
+          return adminData;
+        } else {
+          console.log("No user or admin found with the given userId: ", userId);
+          return null;
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching user or admin details: ", error);
+      throw error;
+    }
+  };
 
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged(async (user) => {
-			setLoading(true);
-			if (user) {
-				const data = await getUserDetails(user.uid);
-				setUserType(data.userType);
-				setIsEmailVerified(user.emailVerified);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      setLoading(true);
+      if (user) {
+        const data = await getUserDetails(user.uid);
+        setUserType(data.userType);
+        setIsEmailVerified(user.emailVerified);
+        setIsEmailVerified(true);
 
-				const allDetails = {
-					...user,
-					data,
-					isEmailVerified: user.emailVerified,
-				};
-				setCurrentUser(allDetails);
-				localStorage.setItem("currentUser", JSON.stringify(allDetails));
-				localStorage.setItem("userType", JSON.stringify(data.userType));
-				localStorage.setItem(
-					"isEmailVerified",
-					JSON.stringify(user.emailVerified),
-				);
-			} else {
-				setCurrentUser(null);
-				setIsEmailVerified(false);
+        const allDetails = {
+          ...user,
+          data,
+          isEmailVerified: user.emailVerified,
+        };
+        setCurrentUser(allDetails);
+        localStorage.setItem("currentUser", JSON.stringify(allDetails));
+        localStorage.setItem("userType", JSON.stringify(data.userType));
+        localStorage.setItem(
+          "isEmailVerified",
+          JSON.stringify(user.emailVerified)
+        );
+      } else {
+        setCurrentUser(null);
+        setIsEmailVerified(false);
 
-				localStorage.removeItem("currentUser");
-				localStorage.removeItem("userType");
-				localStorage.removeItem("isEmailVerified");
-			}
-			setLoading(false);
-		});
+        localStorage.removeItem("currentUser");
+        localStorage.removeItem("userType");
+        localStorage.removeItem("isEmailVerified");
+      }
+      setLoading(false);
+    });
 
-		const storedUser = localStorage.getItem("currentUser");
-		const storedUserType = localStorage.getItem("userType");
-		const storedEmailVerified = localStorage.getItem("isEmailVerified");
+    const storedUser = localStorage.getItem("currentUser");
+    const storedUserType = localStorage.getItem("userType");
+    const storedEmailVerified = localStorage.getItem("isEmailVerified");
 
-		if (storedUser) {
-			setCurrentUser(JSON.parse(storedUser));
-			setUserType(JSON.parse(storedUserType));
-			setIsEmailVerified(JSON.parse(storedEmailVerified));
-		}
-		setLoading(false);
+    if (storedUser) {
+      setCurrentUser(JSON.parse(storedUser));
+      setUserType(JSON.parse(storedUserType));
+      setIsEmailVerified(JSON.parse(storedEmailVerified));
+    }
+    setLoading(false);
 
-		return () => unsubscribe();
-	}, []);
+    return () => unsubscribe();
+  }, []);
 
-	const logout = async () => {
-		try {
-			await auth.signOut();
-			setCurrentUser(null);
-			setUserType(null);
-			setIsEmailVerified(null);
+  const logout = async () => {
+    try {
+      await auth.signOut();
+      setCurrentUser(null);
+      setUserType(null);
+      setIsEmailVerified(null);
 
-			localStorage.removeItem("currentUser");
-			localStorage.removeItem("userType");
-			localStorage.removeItem("isEmailVerified");
-		} catch (error) {
-			console.error("Error logging out: ", error.message);
-		}
-	};
+      localStorage.removeItem("currentUser");
+      localStorage.removeItem("userType");
+      localStorage.removeItem("isEmailVerified");
+    } catch (error) {
+      console.error("Error logging out: ", error.message);
+    }
+  };
 
-	return (
-		<AuthContext.Provider
-			value={{
-				currentUser,
-				loading,
-				logout,
-				userType,
-				isEmailVerified,
-				setIsEmailVerified,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser,
+        loading,
+        logout,
+        userType,
+        isEmailVerified,
+        setIsEmailVerified,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
